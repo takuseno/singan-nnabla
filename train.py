Fix zero-centered gradient penalty

train.py
@@ -21,18 +21,18 @@ def _pad(x, kernel, num_layer):
 def _calc_gradient_penalty(real, fake, discriminator, scope):
     alpha = F.rand(shape=real.shape)
     interpolates = alpha * real + (1.0 - alpha) * fake
+    interpolates.need_grad = True
 
     disc_interpolates = discriminator(x=interpolates, scope=scope)
 
     with nn.parameter_scope(scope):
         params = nn.get_parameters()
 
-    grads = nn.grad([disc_interpolates], params.values())
+    grads = nn.grad([disc_interpolates], [interpolates])
+    norms = [F.sum(g ** 2.0, [1, 2, 3]) ** 0.5 for g in grads]
+    r1_zc_gp = sum([F.mean(norm ** 2.0) for norm in norms])
 
-    norms = [((F.sum(g ** 2.0) ** 0.5) - 1.0) ** 2.0 for g in grads]
-    penalty = sum([F.mean(norm) for norm in norms])
-
-    return penalty
+    return r1_zc_gp
 
 
 def _create_real_images(args):